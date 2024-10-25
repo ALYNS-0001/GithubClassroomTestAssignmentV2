ab5351fc16ce6da886f01fd80d7fe0142ea9e9df
oneillo <oneillo@alleyns.Norg.uk>
2024-10-25 14:05:54 +0100

    Copy over files and test cases from first iter

---
+++ test_file.py
@@ -0,0 +1,14 @@
+from assignment_file import question1
+
+"""
+NOTES for test files:
+-All tests are worth the same number of marks
+-files must be in the repo directory (not subfolders)
+-Test filename must have the prefix or suffix 'test'
+"""
+
+def test1():
+    assert(question1() == 1)
+
+def test2():
+    assert(question1() < 5)
